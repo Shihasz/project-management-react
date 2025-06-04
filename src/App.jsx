fe99d3a20bdea24dd272e3093aba4f9df4b27a2f
Shihasz <shihaspkdst@gmail.com>
2025-06-04 07:02:41 +0530

    adds ProjectsSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
-  return <main className="h-screen my-8"></main>;
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar />
+    </main>
+  );
 }
 
 export default App;
