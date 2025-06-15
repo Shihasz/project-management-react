7521336a73ffcba282e579c600b46173df5434ce
Shihasz <shihaspkdst@gmail.com>
2025-06-15 08:13:33 +0530

    enahnces input elements

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 export default function NewTask({ onAdd }) {
   const [enteredTask, setEnteredTask] = useState("");
+  const [emptyError, setEmptyError] = useState(false);
 
   function handleChange(event) {
     setEnteredTask(event.target.value);
+    setEmptyError(false);
   }
-
+  let cssClasses = "w-64 px-2 py-1 rounded-sm bg-stone-200";
+  if (emptyError) {
+    cssClasses += " border-2 animate-blink-border";
+  }
   function handleClick() {
     if (enteredTask.trim() === "") {
+      setEmptyError(true);
       return;
     }
+    setEmptyError(false);
     onAdd(enteredTask);
     setEnteredTask("");
   }
@@ -18,7 +25,7 @@ export default function NewTask({ onAdd }) {
     <div className="flex items-center gap-4">
       <input
         type="text"
-        className="w-64 px-2 py-1 rounded-sm bg-stone-200"
+        className={cssClasses}
         value={enteredTask}
         onChange={handleChange}
       />
